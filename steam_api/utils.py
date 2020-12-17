6ea1aab56ff90d1057d4049baa6d75068437916e
Frans Johansson <fransjohansson98@gmail.com>
2020-12-17 09:31:58 +0100

    Add HTML cleaning for game descriptions

--- steam_api/utils.py
+++ steam_api/utils.py
@@ -1,4 +1,6 @@
 import re
+from steam_api.api_requests import get_game_data
+from bs4 import BeautifulSoup
 
 
 def id_from_url(url):
@@ -10,3 +12,23 @@ def id_from_url(url):
     if found:
         return found.group(1)
     return None
+
+
+def clean_game_description(id):
+    """
+    Given a game ID, this function returns a cleaned version of its detailed description from the Steam store.
+    The description is assumed to be in HTML format. Hyperlinks are removed, and a full stop is used to separate the text content of the various tags.
+    """
+    try:
+        desc = get_game_data(str(id))['detailed_description']
+    except KeyError as e:
+        print(e)
+        return ''
+
+    soup = BeautifulSoup(desc, 'html.parser')
+
+    # Remove <a> tags with URLs
+    for a in soup.find_all('a', href=True):
+        a.extract()
+
+    return soup.get_text(separator='. ')
