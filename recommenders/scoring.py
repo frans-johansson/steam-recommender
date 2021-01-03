10c682e5d964a258d6f7795a5c379493dfaa021b
Frans Johansson <fransjohansson98@gmail.com>
2021-01-03 19:17:34 +0100

    Housekeeping

--- recommenders/scoring.py
+++ recommenders/scoring.py
@@ -1,17 +1,5 @@
 import pandas as pd
 
-
-def playtime_ratio_scorer(estimator, X, y):
-    """
-    For use with the novelty detector in this module.
-    Will attempt to partition the novelty predictions such that the playtime `y` in the inliers is consistently high and consistently low in the outliers
-    """
-    pred = estimator.fit_predict(X)
-    inliers = y[pred > 0]
-    outliers = y[pred < 0]
-    return inliers.mean() / (inliers.std() * outliers.mean())
-
-
 def get_ratio(test_data, users_games, rec_games):
     all_games = test_data.columns
     tot_recommends = list(set(all_games).intersection(rec_games))
