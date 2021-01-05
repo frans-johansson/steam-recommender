55af930e627cfa976561ba1e34e90249b9201e01
Frans Johansson <fransjohansson98@gmail.com>
2021-01-05 17:42:23 +0100

    Fix scoring method

--- recommenders/scoring.py
+++ recommenders/scoring.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def get_ratio(test_data, users_games, rec_games):
     all_games = test_data.columns
-    tot_recommends = list(set(all_games).intersection(rec_games))
+    tot_recommends = list(set(users_games).intersection(rec_games))
     user_games_in_test = list(set(all_games).intersection(users_games))
     
     ratio = len(tot_recommends)/len(user_games_in_test)
